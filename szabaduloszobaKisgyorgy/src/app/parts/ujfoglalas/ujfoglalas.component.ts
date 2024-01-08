ef70917dbe34510919a18ac6587a57f8428118e0
KisgyorgyBendeguz <kisgyorgybeni@gmail.com>
2024-01-08 07:01:53 +0100

    Adatfelvetel sikeres (hiba az id-nel)

--- szabaduloszobaKisgyorgy/src/app/parts/ujfoglalas/ujfoglalas.component.ts
+++ szabaduloszobaKisgyorgy/src/app/parts/ujfoglalas/ujfoglalas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BaseService } from 'src/app/services/base.service';
 
 @Component({
   selector: 'app-ujfoglalas',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./ujfoglalas.component.css']
 })
 export class UjfoglalasComponent {
+foglalas:any={}
+foglalasok:any
 
+constructor(private base:BaseService, private router:Router){
+  this.base.getData('foglalasok').subscribe(
+    (adatok)=>this.foglalasok=adatok
+  )
+}
+
+kuldApi(){
+  this.base.addData(this.foglalas).subscribe(
+  {
+    next:()=> this.router.navigate(['/foglalasok']),
+    error:(e)=>console.log(e)
+  }
+  )
+  window.location.reload
+}
 }
