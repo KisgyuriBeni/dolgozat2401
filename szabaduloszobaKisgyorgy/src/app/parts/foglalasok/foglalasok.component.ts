8aff0fc8f0c8d1a2f087d1bb00ef4cc6ad9c997c
KisgyorgyBendeguz <kisgyorgybeni@gmail.com>
2024-01-08 01:03:20 +0100

    Fooldal kesz

--- szabaduloszobaKisgyorgy/src/app/parts/foglalasok/foglalasok.component.ts
+++ szabaduloszobaKisgyorgy/src/app/parts/foglalasok/foglalasok.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./foglalasok.component.css']
 })
 export class FoglalasokComponent {
+  id:any
+  cim:any
+  datum:any
+  fo:any
+  iranyitoszam:any
+  nev:any
 
+  oszlopok=[
+    {key:"cim", text:"Cím"},
+    {key:"datum", text:"Dátum"},
+    {key:"fo", text:"Fő"},
+    {key:"iranyitoszam", text:"Irányítószám"},
+    {key:"nev", text:"Név"},
+  ]
 }
